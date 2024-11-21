41c3a37dfb4e806496c5d9182e0983e90c598c27
simon njoroge <mukirisimon22@gmail.com>
2024-11-21 15:11:15 +0300

    initial

--- active/active/settings.py
+++ active/active/settings.py
@@ -82,13 +82,9 @@ WSGI_APPLICATION = 'active.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'active',
-        'USER':'root',
-        'PASSWORD':'',
-        'HOST':'127.0.0.1',
-        'PORT':'3306'
+   'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
