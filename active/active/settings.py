7ab436d059592e4124af39cc9641e0c31d14cd73
simon njoroge <mukirisimon22@gmail.com>
2024-11-21 16:15:09 +0300

    inital

--- active/active/settings.py
+++ active/active/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+STATIC_ROOT='staticfiles/'
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
